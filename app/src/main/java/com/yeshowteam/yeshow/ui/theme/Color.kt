4559d7f60c70529e0898c5d25589ae643ff173d9
wugusaud77 <wugusaud77@icloud.com>
2022-10-30 14:35:06 +0900

    refactor:removed unused color variables

--- app/src/main/java/com/yeshowteam/yeshow/ui/theme/Color.kt
+++ app/src/main/java/com/yeshowteam/yeshow/ui/theme/Color.kt
@@ -2,11 +2,7 @@ package com.yeshowteam.yeshow.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
 val White = Color(0xffffffff)
-val Purple500 = Color(0xFF3DAB70)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 val Blue = Color(0xFF466BED)
 val PrimaryWeek = Color(0xFFEBF9F1)
 val PrimaryColor = Color(0xFF3DAB70)
